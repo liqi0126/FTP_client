f9a8222171a224023969757d38cb728c33e1eae7
liqi17-thu <624275030@qq.com>
2020-10-18 09:52:35 +0800

    update

--- controller.py
+++ controller.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QFileSystemModel, QTableWidgetItem
 
 from PyQt5.QtCore import QAbstractTableModel
 
+from model import SYSTEM_HEADER
+
 
 class ResponseModel(QAbstractTableModel):
     def __init__(self, *args, **kwargs):
@@ -15,12 +17,17 @@ class ClientCtrl(QtCore.QObject):
         self.model = model
         self.view = view
 
-        self.view.responses.setColumnCount(3)
-        self.view.responses.setHorizontalHeaderLabels(['source', 'message', 'code'])
-        self.view.responses.setColumnWidth(0, 80)
-        self.view.responses.setColumnWidth(2, 80)
+        # DEBUG
+        self.view.host.setText("209.51.188.20")
+        self.view.username.setText("anonymous")
+        self.view.password.setText("anonymous@")
+        self.view.port.setText("21")
 
         self.connect_signals()
+
+    @staticmethod
+    def get_status_code(msg):
+        return msg.split(' ')[1]
 
     def connect_signals(self):
         # login and exit
@@ -46,25 +53,16 @@ class ClientCtrl(QtCore.QObject):
         self.view.download.clicked.connect(self.download)
 
     def push_response(self, response):
-        response = response.split(' ')
-        source = response[0]
-        code = response[1]
-        message = ' '.join(response[2:])
-
-        self.view.responses.setRowCount(self.responses.rowCount() + 1)
-        sourceItem = QTableWidgetItem(source)
-        self.view.responses.setItem(self.view.responses.rowCount()-1, 0, sourceItem)
-        messageItem = QTableWidgetItem(message)
-        self.view.responses.setItem(self.view.responses.rowCount()-1, 1, messageItem)
-        codeItem = QTableWidgetItem(code)
-        self.view.responses.setItem(self.view.responses.rowCount()-1, 2, codeItem)
+        if not response.endswith('\n'):
+            response += '\n'
+        self.view.responses.insertPlainText(response)
 
     def push_responses(self, responses):
         if isinstance(responses, list):
             for response in responses:
-                self.add_response(response)
+                self.push_response(response)
         else:
-            self.add_response(responses)
+            self.push_response(responses)
 
     def login(self):
         host = self.view.host.text()
@@ -72,16 +70,32 @@ class ClientCtrl(QtCore.QObject):
         username = self.view.username.text()
         password = self.view.password.text()
 
+        if not host:
+            self.push_responses(SYSTEM_HEADER + "5 please enter host to connect!")
+            return
+
+        if not port:
+            self.push_responses(SYSTEM_HEADER + "5 please enter port to connect!")
+            return
+
         response = self.model.connect(host, port)
         self.push_responses(response)
 
-        if response[0] == '5':
+        if self.get_status_code(response)[0] == '5':
+            return
+
+        if not username:
+            self.push_responses(SYSTEM_HEADER + "5 please enter username to login!")
             return
 
         response = self.model.user(username)
         self.push_responses(response)
 
-        if response[0] == '5':
+        if self.get_status_code(response)[0] == '5':
+            return
+
+        if not password:
+            self.push_responses(SYSTEM_HEADER + "5 please enter password to login!")
             return
 
         response = self.model.password(password)
--- model.py
+++ model.py
@@ -4,9 +4,12 @@ from enum import Enum
 import os
 import re
 
-BUF_SIZE = 8096
+BUF_SIZE = 8192
 BACKLOG = 5
 
+CRLF = '\r\n'
+SERVER_HEADER = 'server: '
+SYSTEM_HEADER = 'system: '
 
 class ClientStatus(Enum):
     DISCONNECT = 0
@@ -20,6 +23,7 @@ class ClientStatus(Enum):
 class ClientModel(object):
     def __init__(self):
         self.command_socket = None
+        self.command_recevier = None
 
         self.status = ClientStatus.DISCONNECT
 
@@ -31,20 +35,54 @@ class ClientModel(object):
 
         self.offset = 0
 
-    # standard command of FTP
-    def connect(self, host, port):
-        self.command_socket = socket(AF_INET, SOCK_STREAM)
-        self.command_socket.connect((host, int(port)))
-        response = str(self.command_socket.recv(BUF_SIZE))
-        return "server " + response
-
-    def send_command(self, command, argu=None):
+    # help functions to communicate with server
+    def push_command(self, command, argu):
         msg = command
         if argu is not None and len(argu) > 0:
             msg += " " + argu
-        msg += "\r\n"
-        self.command_socket.send(msg.encode())
-        return "server " + self.command_socket.recv(BUF_SIZE).decode()
+        msg += CRLF
+        self.command_socket.sendall(msg.encode())
+
+    def getline(self):
+        line = self.command_recevier.readline(BUF_SIZE + 1)
+        if not line:
+            raise EOFError
+        if line[-2:] == CRLF:
+            line = line[:-2]
+        elif line[-1:] in CRLF:
+            line = line[:-1]
+        return line
+
+    def recv_response(self):
+        line = self.getline()
+        if line[3:4] == '-':
+            code = line[:3]
+            while 1:
+                nextline = self.getline()
+                line = line + ('\n' + nextline)
+                if nextline[:3] == code and \
+                        nextline[3:4] != '-':
+                    break
+        return line
+
+    def send_command(self, command, argu=None):
+        self.push_command(command, argu)
+        return SERVER_HEADER + self.recv_response()
+
+    # standard command of FTP
+    def connect(self, ip, port):
+        if not self.is_valid_ipv4_by_ip_and_port(ip, port):
+            return SYSTEM_HEADER + "5 invalid ip address."
+
+        self.command_socket = socket(AF_INET, SOCK_STREAM)
+        try:
+            self.command_socket = create_connection((ip, int(port)))
+        except ConnectionRefusedError:
+            return SYSTEM_HEADER + "5 fail to connect target computer."
+        self.command_recevier = self.command_socket.makefile('r')
+
+        response = self.recv_response()
+        return SERVER_HEADER + response
 
     def user(self, username):
         self.status = ClientStatus.USER
@@ -79,6 +117,7 @@ class ClientModel(object):
         return self.send_command("SYST")
 
     def rest(self, offset):
+        self.offset = offset
         return self.send_command("REST", offset)
 
     def quit(self):
@@ -87,39 +126,61 @@ class ClientModel(object):
         self.command_socket.close()
         return response
 
-    def port(self, ip, port):
-        port = int(port)
+    def port(self):
+        self.file_socket = None
+        for res in getaddrinfo(None, 0, self.command_socket.family, SOCK_STREAM, 0, AI_PASSIVE):
+            af, socktype, proto, canonname, sa = res
+            try:
+                self.file_socket = socket(af, socktype, proto)
+                self.file_socket.bind(sa)
+            except OSError as _:
+                if self.file_socket:
+                    self.file_socket.close()
+                self.file_socket = None
+                continue
+            break
+
+        if self.file_socket is None:
+            return SERVER_HEADER + "fail to bind socket."
+
+        self.file_socket.listen(1)
+        port = self.file_socket.getsockname()[1]  # Get proper port
+        ip = self.command_socket.getsockname()[0]  # Get proper ip
+
+        # port = int(port)
         addr = self.ip_and_port_to_addr(ip, port)
-        self.file_socket = socket(AF_INET, SOCK_STREAM)
-        self.file_socket.bind((ip, port))
-        self.file_socket.listen(BACKLOG)
+        # self.file_socket = socket(AF_INET, SOCK_STREAM)
+        # self.file_socket.bind((ip, port))
+        # self.file_socket.listen(BACKLOG)
         response = self.send_command("PORT", addr)
         self.status = ClientStatus.PORT
         return response
 
     def pasv(self):
         response = self.send_command("PASV")
-        addr = re.search(r"\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3}", response).group()
+        addr = re.search(r"\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3},\d{1,3}", response).group()
+        if not self.is_valid_ipv4_by_addr(addr):
+            return SYSTEM_HEADER + "5 invalid ip address."
         ip, port = self.addr_to_ip_and_port(addr)
         self.file_ip = ip
         self.file_port = port
-        # self.file_socket = socket(AF_INET, SOCK_STREAM)
+        self.file_socket = socket(AF_INET, SOCK_STREAM)
         self.status = ClientStatus.PASV
         return response
 
     def retr_port(self, filename):
         responses = []
 
-        msg = "RETR " + filename + "\r\n"
+        msg = "RETR " + filename + CRLF
         self.command_socket.send(msg.encode())
         socket, _ = self.file_socket.accept()
-        response = "server " + str(self.command_socket.recv(BUF_SIZE))
+        response = SERVER_HEADER + self.recv_response()
         responses.append(response)
 
         if response[0] != '5':
             file_path = os.path.join(self.cur_path, filename)
             self.recv_data(socket, file_path, self.offset)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + self.recv_response()
             responses.append(response)
 
         socket.close()
@@ -130,16 +191,16 @@ class ClientModel(object):
     def retr_pasv(self, filename):
         responses = []
 
-        msg = "RETR " + filename + "\r\n"
+        msg = "RETR " + filename + CRLF
         self.command_socket.send(msg.encode())
         self.file_socket.connect((self.file_ip, self.file_port))
-        response = "server " + str(self.command_socket.recv(BUF_SIZE))
+        response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
         responses.append(response)
 
         if response[0] != '5':
             file_path = os.path.join(self.cur_path, filename)
             self.recv_data(self.file_socket, file_path, self.offset)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
             responses.append(response)
 
         self.file_socket.close()
@@ -152,7 +213,7 @@ class ClientModel(object):
         elif self.status == ClientStatus.PASV:
             response = self.retr_pasv(filename)
         else:
-            return "system 0 RETR require PORT/PASV mode"
+            return SYSTEM_HEADER + "5 RETR require PORT/PASV mode."
         self.offset = 0
         self.status = ClientStatus.PASS
         return response
@@ -161,10 +222,10 @@ class ClientModel(object):
         list_str = b''
         responses = []
 
-        msg = "LIST\r\n"
+        msg = "LIST" + CRLF
         self.command_socket.send(msg.encode())
         socket, _ = self.file_socket.accept()
-        response = "server " + str(self.command_socket.recv(BUF_SIZE).decode())
+        response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE).decode())
         responses.append(response)
 
         if response[0] != '5':
@@ -172,7 +233,7 @@ class ClientModel(object):
             while buf:
                 list_str += buf
                 buf = socket.recv(BUF_SIZE)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
             responses.append(response)
 
         socket.close()
@@ -184,10 +245,10 @@ class ClientModel(object):
         list_str = b''
         responses = []
 
-        msg = "LIST\r\n"
+        msg = "LIST" + CRLF
         self.command_socket.send(msg.encode())
         self.file_socket.connect((self.file_ip, self.file_port))
-        response = "server " + str(self.command_socket.recv(BUF_SIZE))
+        response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
         responses.append(response)
 
         if response[0] != '5':
@@ -195,7 +256,7 @@ class ClientModel(object):
             while buf:
                 list_str += buf
                 buf = self.file_socket.recv(BUF_SIZE)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
             responses.append(response)
 
         self.file_socket.close()
@@ -208,7 +269,7 @@ class ClientModel(object):
         elif self.status == ClientStatus.PASV:
             response, list_str = self.list_pasv()
         else:
-            return "system 0 LIST require PORT/PASV mode", ""
+            return SYSTEM_HEADER + "5 LIST require PORT/PASV mode.", ""
         self.status = ClientStatus.PASS
         return response, list_str
 
@@ -216,19 +277,19 @@ class ClientModel(object):
         file_path = os.path.join(self.cur_path, filename)
 
         if not os.path.isfile(file_path):
-            return "system 0 not such file"
+            return SYSTEM_HEADER + "5 not such file"
 
         responses = []
 
-        msg = "STOR " + filename + "\r\n"
+        msg = "STOR " + filename + CRLF
         self.command_socket.send(msg.encode())
         socket, _ = self.file_socket.accept()
-        response = "server " + str(self.command_socket.recv(BUF_SIZE))
+        response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
         responses.append(response)
 
         if response[0] != '5':
             self.send_data(socket, file_path, self.offset)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
             responses.append(response)
 
         socket.close()
@@ -240,18 +301,18 @@ class ClientModel(object):
         file_path = os.path.join(self.cur_path, filename)
 
         if not os.path.isfile(file_path):
-            return "system 0 not such file"
+            return SYSTEM_HEADER + "5 not such file."
 
         responses = []
-        msg = "STOR " + filename + "\r\n"
+        msg = "STOR " + filename + CRLF
         self.command_socket.send(msg.encode())
         self.file_socket.connect((self.file_ip, self.file_port))
-        response = "server " + str(self.command_socket.recv(BUF_SIZE))
+        response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
         responses.append(response)
 
         if response[0] != '5':
             self.send_data(socket, file_path, self.offset)
-            response = "server " + str(self.command_socket.recv(BUF_SIZE))
+            response = SERVER_HEADER + str(self.command_socket.recv(BUF_SIZE))
             responses.append(response)
 
         self.file_socket.close()
@@ -264,17 +325,36 @@ class ClientModel(object):
         elif self.status == ClientStatus.PASV:
             response = self.stor_pasv(filename)
         else:
-            return "system 0 STOR require PORT/PASV mode"
+            return SYSTEM_HEADER + "5 STOR require PORT/PASV mode."
         self.offset = 0
         self.status = ClientStatus.PASS
         return response
 
     # help functions
     @staticmethod
+    def is_valid_ipv4_by_addr(addr):
+        addr_num = addr.split(',')
+        if len(addr_num) != 6:
+            return False
+
+        for num in addr_num:
+            try:
+                num = int(num)
+            except:
+                return False
+            if num < 0 or num > 255:
+                return False
+        return True
+
+    def is_valid_ipv4_by_ip_and_port(self, ip, port):
+        addr = self.ip_and_port_to_addr(ip, port)
+        return self.is_valid_ipv4_by_addr(addr)
+
+    @staticmethod
     def addr_to_ip_and_port(addr):
         addr = addr.split(',')
         if len(addr) < 6:
-            return "system 0 invalid addr\r\n"
+            return SYSTEM_HEADER + "5 invalid ip address."
         ip = '.'.join(addr[:4])
         port = int(addr[-2]) * 256 + int(addr[-1])
         return ip, port
@@ -282,15 +362,19 @@ class ClientModel(object):
     @staticmethod
     def ip_and_port_to_addr(ip, port):
         addr = ip.split('.')
-        addr.append(str(port / 256))
+        port = int(port)
+        addr.append(str(port // 256))
         addr.append(str(port % 256))
         return ','.join(addr)
 
     @staticmethod
     def recv_data(sock, file_path, offset):
-        fp = open(file_path, "wb")
+
         if offset > 0:
+            fp = open(file_path, "r+b")
             fp.seek(offset-1, 0)
+        else:
+            fp = open(file_path, "wb")
 
         buf = sock.recv(BUF_SIZE)
         while buf:
@@ -308,3 +392,62 @@ class ClientModel(object):
         while buf:
             sock.send(buf)
             fp.read(BUF_SIZE)
+
+
+def test_login(ftp, client):
+    fr1 = ftp.connect("209.51.188.20", 21)
+    fr2 = ftp.sendcmd("USER anonymous")
+    fr3 = ftp.sendcmd("PASS anonymous@")
+
+    cr1 = client.connect("209.51.188.20", 21)
+    cr2 = client.send_command("USER anonymous")
+    cr3 = client.send_command("PASS anonymous@")
+
+    assert fr1 == cr1[8:]
+    assert fr2 == cr2[8:]
+    assert fr3 == cr3[8:]
+
+
+def test_dir_manipulate(ftp, client):
+    pass
+
+
+def test_file_retr(ftp, client, filename):
+    import filecmp
+    ftp.retrbinary(f"RETR {filename}", open("ftp_retr", 'wb').write)
+
+    client.send_command("TYPE I")
+    client.port()
+    client.retr(filename)
+
+    assert filecmp.cmp("ftp_retr", filename)
+    os.remove(filename)
+
+    client.send_command("TYPE I")
+    client.pasv()
+    client.retr(filename)
+    assert filecmp.cmp("ftp_retr", filename)
+    os.remove(filename)
+
+
+def test_file_stor(ftp, client, filename):
+    pass
+
+
+if __name__ == '__main__':
+    from ftplib import FTP
+    ftp = FTP()
+
+    client = ClientModel()
+
+    test_login(ftp, client)
+
+    # test_file_retr(ftp, client, "CRYPTO.README")
+
+    print(ftp.dir())
+    ftp.mkd("temp")
+    # ftp.storbinary("STOR README.md", open("README.md", "rb"))
+
+    print()
+    print(ftp.dir())
+
